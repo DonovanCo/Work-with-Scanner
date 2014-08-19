02e0a7971b474a7ccf91e0b4e3c78d8374feb547
Cole.Donovan <Cole.Donovan@MU-C3-10.mullen.pvt.k12.co.us>
2014-08-19 14:31:03 -0600

    Initial commit

---
+++ Work with Scanner/src/ScannerPractice.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class ScannerPractice {
+
+	public static void main(String[] args) {
+		Scanner scannerPractice = new Scanner(System.in);
+		
+		String name;
+		String favAnimal;
+		String Cat;
+		
+		System.out.println("What is your name?");
+		name = scannerPractice.nextLine();
+		System.out.println("Nice to meet you" + name + ".");
+		
+		System.out.println("What is your favorite animal? a) cat b) dog c)other");
+		favAnimal = scannerPractice.nextLine();
+		if()
+		
+		
+		
+
+	}
+
+}
